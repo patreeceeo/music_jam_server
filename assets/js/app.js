f478e65406515542a8d4b5a5bbd784c0911740d4
Patrick <pscale01@gmail.com>
2022-05-01 18:52:35 -0700

    install webaudiofont js lib & types

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,12 +19,14 @@ import {channel} from "./user_socket.js"
 //     import "some-package"
 //
 
+
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
