65b63b9095940d39b5b66d8db5d4a7f7b14cba11
Patrick <pscale01@gmail.com>
2022-06-07 17:30:09 -0700

    try something

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,77 +44,79 @@ function $(selector) {
   return Array.prototype.slice.apply(document.querySelectorAll(selector))
 }
 
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-topbar.show()
-const promises = [
-  player.adjustPreset(audioContext, sf2_guitar)
-]
+document.addEventListener("DOMContentLoaded", () => {
+  topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+  topbar.show()
+  const promises = [
+    player.adjustPreset(audioContext, sf2_guitar)
+  ]
 
 
-const elmApp = Elm.Main.init(config);
+  const elmApp = Elm.Main.init(config);
 
 
-Promise.all(promises).then(() => {
-  topbar.progress(1)
-  topbar.hide()
-  console.timeEnd("adjustPreset")
-  var gainDrums = audioContext.createGain();gainDrums.connect(audioContext.destination);gainDrums.gain.value=0.5;
-  var gainSynth = audioContext.createGain();gainSynth.connect(audioContext.destination);gainSynth.gain.value=0.3;
-  var gainBass = audioContext.createGain();gainBass.connect(audioContext.destination);gainBass.gain.value=0.7;
-  var gainHit = audioContext.createGain();gainHit.connect(audioContext.destination);gainHit.gain.value=0.5;
+  Promise.all(promises).then(() => {
+    topbar.progress(1)
+    topbar.hide()
+    console.timeEnd("adjustPreset")
+    var gainDrums = audioContext.createGain();gainDrums.connect(audioContext.destination);gainDrums.gain.value=0.5;
+    var gainSynth = audioContext.createGain();gainSynth.connect(audioContext.destination);gainSynth.gain.value=0.3;
+    var gainBass = audioContext.createGain();gainBass.connect(audioContext.destination);gainBass.gain.value=0.7;
+    var gainHit = audioContext.createGain();gainHit.connect(audioContext.destination);gainHit.gain.value=0.5;
 
-  elmApp.ports.sendPortMessage.subscribe((msg) => {
-    switch(msg.type) {
-      case "playSound":
-        pushUpdate(msg.data)
-        break;
-      case "logError":
-        console.error(msg.data.message)
-        break;
+    elmApp.ports.sendPortMessage.subscribe((msg) => {
+      switch(msg.type) {
+        case "playSound":
+          pushUpdate(msg.data)
+          break;
+        case "logError":
+          console.error(msg.data.message)
+          break;
+      }
+    })
+
+    function playNote(gain, preset, pitch, duration, volume) {
+      player.playNote(audioContext, gain, preset, pitch, duration, volume);
     }
-  })
 
-  function playNote(gain, preset, pitch, duration, volume) {
-    player.playNote(audioContext, gain, preset, pitch, duration, volume);
-  }
+    function playGuiar(pitch, duration, volume) {
+      playNote(gainBass, sf2_guitar, pitch, duration, volume);
+    }
 
-  function playGuiar(pitch, duration, volume) {
-    playNote(gainBass, sf2_guitar, pitch, duration, volume);
-  }
-
-  const playVoiceBtns = $("[data-role=play-voice]")
-  playVoiceBtns.map((button) => button.addEventListener("mousedown", downListener))
-  playVoiceBtns.map((button) => button.addEventListener("mouseover", overListener))
+    const playVoiceBtns = $("[data-role=play-voice]")
+    playVoiceBtns.map((button) => button.addEventListener("mousedown", downListener))
+    playVoiceBtns.map((button) => button.addEventListener("mouseover", overListener))
 
 
-  let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-  let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
-  let socket = new Socket("/socket", {params: {token: win.userToken}})
+    let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+    let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+    let socket = new Socket("/socket", {params: {token: win.userToken}})
 
 
-  liveSocket.connect()
-  socket.connect()
+    liveSocket.connect()
+    socket.connect()
 
-  function pushUpdate(event) {
-    channel.push("playSound", event)
-  }
+    function pushUpdate(event) {
+      channel.push("playSound", event)
+    }
 
-  function downListener(event) {
-    pushUpdate(event)
-  }
-  function overListener(event) {
-    if(event.buttons > 0) {
+    function downListener(event) {
       pushUpdate(event)
     }
-  }
-  const voiceInfo = {};
-  channel.on("playSound", (event) => {
-    if(event.volume > 0) {
-      playGuiar(event.pitch, 5, event.volume)
-      elmApp.ports.receivePortMessage.send({
-        type: "playSound",
-        data: event
-      })
+    function overListener(event) {
+      if(event.buttons > 0) {
+        pushUpdate(event)
+      }
     }
+    const voiceInfo = {};
+    channel.on("playSound", (event) => {
+      if(event.volume > 0) {
+        playGuiar(event.pitch, 5, event.volume)
+        elmApp.ports.receivePortMessage.send({
+          type: "playSound",
+          data: event
+        })
+      }
+    })
   })
 })
