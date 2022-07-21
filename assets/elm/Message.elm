0394ce74ffd5721badad9792fd69c4fc7c8814b7
Patrick <pscale01@gmail.com>
2022-07-21 12:50:15 -0700

    wip: test interpret message fn

--- assets/elm/Message.elm
+++ assets/elm/Message.elm
@@ -1,9 +1,9 @@
-module Message exposing (Message(..))
+module Message exposing (Context(..), Message(..), interpret)
 
 import Browser
 import Browser.Events
 import Chord
-import CommonTypes exposing (ClientId, Inputs, NoteVIndex, Pitch, Volume)
+import CommonTypes exposing (ClientId, Inputs, NoteVIndex, Pitch, Routes(..), Volume)
 import KbdEvent
 import MouseEvent
 import PortMessage
@@ -28,3 +28,80 @@ type Message
     | RequestPreviousUrl Int
     | FocusChange Inputs
     | Nevermind
+
+
+type Context
+    = WithRoute Routes
+    | WithMaybeUrl (Maybe Url)
+    | WithClientId ClientId
+    | WithRouteAndVolume Routes Volume
+    | WithRouteVolumeNoteVIndexAndPitch Routes Volume NoteVIndex Pitch
+    | WithRouteAndSequenceItem Routes Chord.Names
+
+
+
+-- TODO(optimize): find way to reduce the number of parameters. One way would be including the necessary info in the corresponding variants of an abstract type that wraps Message, then use that instead of Message.
+
+
+interpret : Message -> Context -> List Message
+interpret msg context =
+    case ( msg, context ) of
+        ( ReceivePortMessage sender payload, WithClientId clientId ) ->
+            -- Ignore messages sent to self
+            if sender == clientId then
+                []
+
+            else
+                [ ReceivePortMessage sender payload ]
+
+        ( KeyDown event, WithMaybeUrl maybeUrl ) ->
+            case event.key of
+                KbdEvent.KeyEnter ->
+                    maybeUrl
+                        |> Maybe.map (\url -> [ msg, UrlRequest (Browser.Internal url) ])
+                        |> Maybe.withDefault [ msg ]
+
+                _ ->
+                    [ msg ]
+
+        ( KeyUp { key }, WithRouteAndVolume MainRoute intensityOfPress ) ->
+            case key of
+                KbdEvent.KeySpace ->
+                    [ msg, PlayChord intensityOfPress ]
+
+                _ ->
+                    [ msg ]
+
+        ( KeyUp _, WithRouteVolumeNoteVIndexAndPitch MainRoute volume noteVIndex intensityOfPress ) ->
+            [ msg, PlayNote volume noteVIndex intensityOfPress ]
+
+        ( KeyDown _, WithRouteAndSequenceItem SelectChordRoute chord ) ->
+            [ msg, SelectChord chord ]
+
+        _ ->
+            [ msg ]
+
+
+
+--      ( SelectChordRoute, Message.KeyDown event ) ->
+--          let
+--              userAction =
+--                  userActionForKey event.key
+--          in
+--          case userAction of
+--              User.Interface.Dismiss ->
+--                  [ msg, Message.RequestPreviousUrl 1 ]
+--              _ ->
+--                  let
+--                      newChordName =
+--                          case userAction of
+--                              User.Interface.SeekForward ->
+--                                  Chord.next model.uiInstrument.activeChordName
+--                              User.Interface.SeekBackward ->
+--                                  Chord.previous model.uiInstrument.activeChordName
+--                              _ ->
+--                                  model.uiInstrument.activeChordName
+--                  in
+--                  [ msg, Message.SelectChord newChordName ]
+--      _ ->
+--          [ msg ]
